4b67e6df723c580660319f21607d6b459db9f0a6
WIlliam Silva <wyllyambmg@gmail.com>
2017-03-29 16:02:09 -0300

    first commit

---
+++ src/main/java/com/alvorecer/venus/VenusAplication.java
@@ -0,0 +1,15 @@
+package com.alvorecer.venus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class VenusAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusAplication.class, args);
+
+	}
+}
---
+++ src/main/java/com/alvorecer/venus/config/JPAConfig.java
@@ -0,0 +1,53 @@
+package com.alvorecer.venus.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alvorecer.venus.model.Attendance;
+import com.alvorecer.venus.repository.Attendances;
+
+@Configuration
+@EnableJpaRepositories(basePackageClasses = Attendances.class, enableDefaultTransactions = false)
+@ComponentScan(basePackageClasses = Attendances.class)
+@EnableTransactionManagement
+public class JPAConfig {
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+	}
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setJpaVendorAdapter(jpaVendorAdapter);
+		factory.setPackagesToScan(Attendance.class.getPackage().getName());
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+}
